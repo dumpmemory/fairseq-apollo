57c7c2f1b9c3f62e607ddd5b220db5b2ea9aebdf
Xuezhe Ma <max@Xuezhes-MacBook-Pro.local>
2021-07-12 01:23:21 -0700

    cuda kernel

--- fairseq/modules/luna_causal_attention/lca_cuda_kernel.cu
+++ fairseq/modules/luna_causal_attention/lca_cuda_kernel.cu
@@ -17,4 +17,37 @@
 template<typename scalar_t>
 __global__ 
 void lca_cuda_forward_kernel(
-    )
+    const scalar_t* __restrict__ x,
+    const scalar_t* __restrict__ y,
+    const scalar_t* __restrict__ z,
+    scalar_t* __restrict__ f) {
+
+
+}
+
+torch::Tensor lca_cuda_forward(
+    torch::Tensor const& x,
+    torch::Tensor const& y,
+    torch::Tensor const& z) {
+    /*
+     Args:
+         x: [len, bsz, dim1]
+         y: [len, bsz, dim1]
+         z: [len, bsz, dim2]
+     return:
+         f: [len, bsz, dim2]
+    */
+    
+    const int len = x.size(0);
+    const int bsz = x.size(1);
+    const int dim_xy = x.size(2);
+    const int dim_z = z.size(2);
+
+    const int xy_inc_t = bsz * dim_xy;
+    const int z_inc_t = bsz * dim_z;
+
+    auto act_options  = x.options().requires_grad(false);
+    torch::Tensor f = torch::zeros({len, bsz, dim_z}, act_options);
+
+    
+}
